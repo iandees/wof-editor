b3984256966588a50c73fb720385dca837bf4173
Ian Dees <ian.dees@gmail.com>
2020-02-10 13:03:52 -0600

    ProxyFix to help with custom domain name.

--- editor/__init__.py
+++ editor/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from editor.config import config
 
@@ -14,6 +15,7 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=2, x_host=1, x_proto=1)
 
     Bootstrap(app)
     login_manager.init_app(app)
