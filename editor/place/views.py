63a6a9477c4a78b3b1e749c549f73ff6383069b8
burritojustice <hubgit@oram.com>
2020-02-20 14:37:09 -0800

    Update editor/place/views.py
    Co-Authored-By: Ian Dees <ian.dees@gmail.com>

--- editor/place/views.py
+++ editor/place/views.py
@@ -376,7 +376,7 @@ def edit_place():
             apply_change(wof_doc, request.form, "edtf:inception")
             apply_change(wof_doc, request.form, "edtf:superseded")
             apply_change(wof_doc, request.form, "wof:superseded_by")
-            apply_change(wof_doc, request.form, "wof:supersedes")
+            apply_change(wof_doc, request.form, "wof:supersedes", list_of_int)
             apply_change(wof_doc, request.form, "wof:population", maybe_int)
             apply_change(wof_doc, request.form, "mz:min_zoom", maybe_float)
             apply_change(wof_doc, request.form, "mz:max_zoom", maybe_float)
